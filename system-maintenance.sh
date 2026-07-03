42b49bfb2ea2ebd2bad575adf1f9854bf9e8869d
Hao The Phan <44211097+HaoThePhan@users.noreply.github.com>
2026-07-02 20:42:36 -0400

    Update system-maintenance.sh
    fix the error

--- system-maintenance.sh
+++ system-maintenance.sh
@@ -2,7 +2,7 @@
 #Check if the laptop is running on AC power
 if [ -d /sys/class/power_supply ]; then
   #look for any AC adapter that is online(1 = plugged in)
-  if ! grep =q "^1$" /sys/class/power_supply/*/online 2>/dev/null; then
+  if ! grep -q "^1$" /sys/class/power_supply/*/online 2>/dev/null; then
     echo "System is running on battery. Aborting Maintenance to save power"
     exit 0
   fi
