8f36ed2f69375f52bde34cc30cf35d57181a140a
Hao The Phan <44211097+HaoThePhan@users.noreply.github.com>
2026-07-02 20:02:56 -0400

    Create system-maintenance.sh
    sudo nano /usr/local/bin/system-maintenance.sh
    after that
    sudo chmod +x /usr/local/bin/system-maintenance.sh

---
+++ system-maintenance.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Check if the laptop is running on AC power
+if [ -d /sys/class/power_supply ]; then
+  #look for any AC adapter that is online(1 = plugged in)
+  if ! grep =q "^1$" /sys/class/power_supply/*/online 2>/dev/null; then
+    echo "System is running on battery. Aborting Maintenance to save power"
+    exit 0
+  fi
+fi
+
+echo "AC power detected. Starting system maintenance..."
+#Run the update and cleanup chain
+apt update
+apt upgrade -y
+apt autoremove
+apt autoclean
+flatpak update -y
+flatpak uninstall --unused -y
+journalctl --vacuum-time=14d
+
+echo "Maintenance Complete!"
